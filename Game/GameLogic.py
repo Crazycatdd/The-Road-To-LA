29dfa40ab1a8282b3204336f682ed1a2c7e7fc81
Caleb Lopez <calebsantiago5542@gmail.com>
2026-04-24 09:31:46 -0500

    Fifth Commit: FInished cars dictionary and started places dictionary

--- Game/GameLogic.py
+++ Game/GameLogic.py
@@ -11,7 +11,7 @@ def playgame():
     mrgrauesclock.sleep(2)
     Clear_Screen()
 
-    print("You decide to step up by tackling a journey with little to no carbon emmisions")
+    print("You decide to step up by tackling a journey from Chicago to Los Angeles with little to no carbon emmisions")
     input("\nIt's time to choose how you are going to approach this journey [press enter]")
 
     Clear_Screen()
